255c29a9c09e5a51a0cd9327cdceb2e75faf1fd1
CUSTcoding <custodiotitossetitosse@gmail.com>
2025-12-31 20:11:59 -0600

    feat: data type in python

---
+++ Python/basico/aula03/data_types.py
@@ -0,0 +1,67 @@
+"""
+Arquivo: tipos_de_dados_python.py
+Assunto: Tipos de Dados em Python (com print e type)
+Descrição:
+Exemplo simples mostrando os principais tipos de dados
+em Python e seus respectivos tipos.
+"""
+
+# ===============================
+# TIPOS PRIMITIVOS
+# ===============================
+
+# int
+numero_inteiro = 10
+print(numero_inteiro)
+print(type(numero_inteiro))
+
+# float
+numero_decimal = 3.14
+print(numero_decimal)
+print(type(numero_decimal))
+
+# str
+texto = "Olá, Python"
+print(texto)
+print(type(texto))
+
+# bool
+verdadeiro = True
+print(verdadeiro)
+print(type(verdadeiro))
+
+# NoneType (null)
+nulo = None
+print(nulo)
+print(type(nulo))
+
+# ===============================
+# TIPOS DE DADOS COMPOSTOS
+# ===============================
+
+# list (array)
+lista = [1, 2, 3, 4]
+print(lista)
+print(type(lista))
+
+# tuple
+tupla = (10, 20, 30)
+print(tupla)
+print(type(tupla))
+
+# dict
+dicionario = {
+    "nome": "Ana",
+    "idade": 22
+}
+print(dicionario)
+print(type(dicionario))
+
+# set
+conjunto = {1, 2, 3}
+print(conjunto)
+print(type(conjunto))
+
+# ===============================
+# FIM DO ARQUIVOa
+# ===============================
